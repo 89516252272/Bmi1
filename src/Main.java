dc753f5f8a66ff9b698ed9c81d27b42d3c7cc5dd
Asmik <hasohovsepyan@bk.ru>
2022-09-25 11:10:38 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
-
 public class Main {
     public static void main(String[] args) {
-        BmiService service = new BmiService();
-        int weight = 89; // вес в килограммах
-        double height = 1.87; // рост в метрах
-        double bmi = service.calculate(weight, height); // считаем ИМТ
-        String bmiResult = String.format("%.2f", bmi); // округляем до 2-х знаков
-        System.out.println("Ваш вес: " + weight + " кг.");
-        System.out.println("Ваш рост: " + height * 100 + " см.");
-        System.out.println("Ваш индекс массы тела: " + bmiResult);
+        CreditPaymentService service = new CreditPaymentService();
+        double amount = 1_000_000; //общая сумма кредита ;
+        double years = 1;//количество лет кредита
+        double payment = service.calculate(amount, years);// ежемесячный платеж
+        System.out.printf("\n" + "Количество лет:%.0f , Ежемесячный платеж:%.0f руб", years, payment);
+
+
+        amount = 1_000_000; //общая сумма кредита ;
+        years = 2;//количество лет кредита
+        payment = service.calculate(amount, years);// ежемесячный платеж
+        System.out.printf("\n" + "Количество лет:%.0f , Ежемесячный платеж:%.0f руб", years, payment);
+
+        amount = 1_000_000; //общая сумма кредита ;
+        years = 3;//количество лет рабства
+        payment = service.calculate(amount, years);// ежемесячный платеж
+        System.out.printf("\n" + "Количество лет:%.0f , Ежемесячный платеж:%.0f руб", years, payment);
     }
 }
