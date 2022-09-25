63adb1a9707175b7017ef66a396736e8267ffc68
Asmik <hasohovsepyan@bk.ru>
2022-09-25 11:02:43 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+
 public class Main {
     public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
-        int price = 10000;
-        int bonus = service.calculate(price);
-        System.out.println(bonus);
+        BmiService service = new BmiService();
+        int weight = 89; // вес в килограммах
+        double height = 1.87; // рост в метрах
+        double bmi = service.calculate(weight, height); // считаем ИМТ
+        String bmiResult = String.format("%.2f", bmi); // округляем до 2-х знаков
+        System.out.println("Ваш вес: " + weight + " кг.");
+        System.out.println("Ваш рост: " + height * 100 + " см.");
+        System.out.println("Ваш индекс массы тела: " + bmiResult);
     }
 }
-
